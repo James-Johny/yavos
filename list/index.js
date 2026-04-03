6ab2bc521151eb0c24d0240f674ef4a7d60d16c8
James Johny <jamesjohny.sg@gmail.com>
2026-04-03 04:20:17 -0300

    criação da lista de itens

---
+++ list/index.js
@@ -0,0 +1,6 @@
+function adicionarItem(item) {
+    const item = document.getElementById("item").value
+    const itemList = document.getElementById("lista");
+
+    itemList.innerHTML = `<li>item</li>`
+}
