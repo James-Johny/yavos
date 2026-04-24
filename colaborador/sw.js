09b9e7db1419d9c587cbf7ebc1369d7ddc9b411c
James Johny <jamesjohny.sg@gmail.com>
2026-04-24 19:52:43 +0000

    Correção do sw.js

--- colaborador/sw.js
+++ colaborador/sw.js
@@ -1,6 +1,6 @@
 const CACHE_NAME = "v1-colab-cache";
 const ASSETS_TO_CACHE = [
-  "./colaborador/", // Raiz (geralmente index.html)
+  "./", // Raiz (geralmente index.html)
   "index.html",
   "estilos.css",
   "script.js",
