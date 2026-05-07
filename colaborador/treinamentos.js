2029b04191db57f780c50bced4e3ee58ef41315b
James Johny <jamesjohny.sg@gmail.com>
2026-05-07 08:03:29 +0000

    Update 2.11.11

---
+++ colaborador/treinamentos.js
@@ -0,0 +1,214 @@
+const inflamaveis = `
+        <section class="inflamaveis">
+
+            <span id="01" class="pergunta">Questão 01: A NR - 20 estabelecer requisitos mínimos para a gestão da segurança e
+                saúde no trabalho contra os fatores de risco de acidentes provenientes das atividades de;</span>
+            <ul class="respostas">
+                <li class="errada">Extração, produção, armazenamento, transferência, manuseio e manipulação de todos
+                    os produtos químicos.</li>
+                <li class="errada">Extração, produção, armazenamento, transferência, manuseio e manipulação de
+                    materiais líquidos e sólidos.</li>
+                <li class="certa">Extração, produção, armazenamento, transferência, manuseio e manipulação de
+                    inflamáveis e líquidos combustíveis.</li>
+                <li class="errada">Todas as alternativas estão corretas.</li>
+            </ul>
+            <span id="02" class="pergunta">Questão 02: Em relação ao tetraedro do fogo, é correto afirmar que:</span>
+            <ul class="respostas">
+                <li class="errada">Comburente é todo elemento que, associando-se quimicamente ao combustível, é
+                    capaz de fazê-lo entrar em combustão na presença de uma fonte de ignição inicial.</li>
+                <li class="errada">Combustível é uma substância que reage com o oxigênio (ou outro comburente)
+                    liberando energia, usualmente de modo vigoroso, na forma de calor, chamas e gases.</li>
+                <li class="errada">O oxigênio, presente no ar ambiente, é o principal comburente.</li>
+                <li class="certa">Todas as Alternativas estão corretas.</li>
+            </ul>
+            <span id="03" class="pergunta">Questão 03: Marque a alternativa correta sobre conceituação básica.</span>
+            <ul class="respostas">
+                <li class="certa">Área Classificada: Local sujeito a “probabilidade” da formação ou existência de
+                    uma atmosfera explosiva.</li>
+                <li class="errada">Explosão: É a energia mínima que deve ser fornecida por uma chama, centelha
+                    elétrica ou fonte de calor à uma atmosfera explosiva para que esta possa iniciar a propagação da
+                    combustão.</li>
+                <li class="errada">Ignição: É o modo pela qual, após a ignição, a combustão se transmite de forma
+                    autossustentada pela atmosfera explosiva.</li>
+                <li class="errada">Todas as Alternativas estão corretas.</li>
+            </ul>
+            <span id="04" class="pergunta">Questão 04: As fontes de ignição podem ser geradas a partir de:</span>
+            <ul class="respostas">
+                <li class="errada">Velocidade ou direção do vento.</li>
+                <li class="certa">Chama aberta, mecânica, elétrica, eletrostática, temperatura de superfície e auto
+                    ignição. </li>
+                <li class="errada">Enriquecimento de Oxigênio no ambiente.</li>
+                <li class="errada">Todas as alternativas estão corretas.</li>
+            </ul>
+            <span id="05" class="pergunta">Questão 05: Marque a alternativa correta sobre áreas classificadas devido a presença
+                de poeiras e fibras.</span>
+            <ul class="respostas">
+                <li class="errada">Derivados do petróleo como gasolina, óleo diesel e etanol são exemplos de poeira
+                    e fibras</li>
+                <li class="errada">Quanto maior for o tamanho do fragmento, mais rápido será a propagação.</li>
+                <li class="certa">Quanto menor o tamanho do fragmento, mais rápida é a combustão.</li>
+                <li class="errada">Todas as alternativas estão corretas.</li>
+            </ul>
+            <span id="06" class="pergunta">Questão 06: O que é o processo de explosão?</span>
+            <ul class="respostas">
+                <li class="errada">É o processo pela qual, após a ignição, a combustão se transmite de forma
+                    autossustentada pela atmosfera explosiva.</li>
+                <li class="errada">É o processo de propagação de atmosfera explosiva de um líquido inflamável em uma
+                    temperatura ambiente.</li>
+                <li class="certa">É o processo caracterizado por súbito aumento de volume e grande liberação de
+                    energia, geralmente acompanhado por altas temperaturas e produção de gases.</li>
+                <li class="errada">Todas as alternativas estão corretas.</li>
+            </ul>
+            <span id="07" class="pergunta">Questão 07: Quais medidas preventivas devem ser consideradas para reduzir ou eliminar
+                riscos relacionados a substâncias combustíveis e inflamáveis.</span>
+            <ul class="respostas">
+                <li class="errada">Comunicação de riscos, demarcações de área, controle de acesso e carga horária.
+                </li>
+                <li class="errada">Fatores ergonômicos, aplicação do OPA e isolamento de área.</li>
+                <li class="certa">Identificação do risco, controle da atmosfera, controle da ignição e controle de
+                    danos.</li>
+                <li class="errada">Todas as alternativas estão corretas.</li>
+            </ul>
+            <span id="08" class="pergunta">Questão 08: Marque a alternativa correta sobre líquidos inflamáveis e combustíveis
+                inflamáveis.</span>
+            <ul class="respostas">
+                <li class="errada">Líquidos inflamáveis são todos aqueles que possuem um ponto de fulgor inferior ou
+                    igual a 60ºC, ou ainda, que em certas condições de temperatura e pressão entram em combustão com
+                    facilidade, podem sair do estado líquido e virar chama.</li>
+                <li class="errada">Combustíveis inflamáveis são aqueles que possuem um ponto de fulgor superior a
+                    60ºC e inferior a 93ºC. É necessária a interferência de um fator externo para que haja combustão,
+                    como ar, acúmulo de gás ou vapor inflamável e fontes de ignição.</li>
+                <li class="errada">São exemplos de líquidos inflamáveis o acetileno, solvente, gasolina, benzeno e
+                    detergentes sintéticos.</li>
+                <li class="certa">Todas as alternativas estão corretas.</li>
+            </ul>
+            <span id="09" class="pergunta">Questão 09: Marque a alternativa correta sobre ponto de fulgor e ponto de
+                ebulição.</span>
+            <ul class="respostas">
+                <li class="certa">Ponto de fulgor é a temperatura mínima necessária para que um combustível
+                    desprenda vapores inflamáveis.</li>
+                <li class="errada">Ponto de fulgor é a temperatura em que uma substância atinge e mudar do estado
+                    líquido para o estado gasoso.</li>
+                <li class="errada">Ponto de ebulição é a temperatura mínima necessária para que um combustível
+                    desprenda vapores inflamáveis.</li>
+                <li class="errada">Todas as alternativas estão corretas.</li>
+            </ul>
+            <span id="10" class="pergunta">Questão 10: As instalações de armazenamento de combustíveis são classificadas pela
+                NR20 em: </span>
+            <ul class="respostas">
+                <li class="errada">Grupo 1, Grupo 2 e Grupo 3.</li>
+                <li class="certa">Classe I, Classe II e Classe III.</li>
+                <li class="errada">Sessão I, Sessão 2 e Sessão 3.</li>
+                <li class="errada">Todas as alternativas, estão corretas.</li>
+            </ul>
+            <span id="11" class="pergunta">Questão 11: Marque a alternativa que corresponde corretamente ao controle da
+                atmosfera.</span>
+            <ul class="respostas">
+                <li class="errada">Trata-se de precauções tomadas para evitar ou restringir a formação de atmosferas
+                    explosivas perigosas.</li>
+                <li class="errada">Identifica a possibilidade de ocorrência da atmosfera explosiva, estuda e adota
+                    medidas que possam reduzir/eliminar esta possibilidade.</li>
+                <li class="errada">As medidas podem ser, desde a aplicação de procedimentos adequados, como a
+                    inertização de tanques e vasos. </li>
+                <li class="certa">Todas as alternativas estão corretas.</li>
+            </ul>
+            <span id="12" class="pergunta">Questão 12: Sobre o controle da ignição, é correto afirmar que;</span>
+            <ul class="respostas">
+                <li class="errada">São medidas de controle que devem ser tomadas para evitar a possibilidade de
+                    ocorrência de fontes de ignição simultaneamente com a ocorrência desta atmosfera explosiva.</li>
+                <li class="errada">Controle da ignição é o ato de substituir os recipientes de armazenamento de
+                    substâncias inflamáveis e explosivas por recipientes fechados.</li>
+                <li class="errada">Devem ser adotas antes mesmo de quaisquer medidas para controle da atmosfera.
+                </li>
+                <li class="certa">Todas as alternativas estão corretas.</li>
+            </ul>
+            <span id="13" class="pergunta">Questão 13: Quais ferramentas são utilizadas para comunicar riscos químicos de forma
+                eficaz?</span>
+            <ul class="respostas">
+                <li class="errada">Fichas de segurança de produtos químicos (FISPQ)</li>
+                <li class="errada">Rótulos e sinalizações de segurança</li>
+                <li class="errada">Treinamentos e palestras sobre segurança química</li>
+                <li class="certa">Todas as alternativas estão corretas.</li>
+            </ul>
+
+            <span id="14" class="pergunta">Questão 14: O que é uma FISPQ? (Ficha de Informação de Segurança para Produtos
+                Químicos)</span>
+            <ul class="respostas">
+                <li class="errada">Documento de venda de produtos químicos</li>
+                <li class="certa">Documento que fornece informações sobre perigos, precauções e medidas de
+                    primeiros socorros de um produto químico</li>
+                <li class="errada">Receita para misturar produtos químicos</li>
+                <li class="errada">Todas as alternativas estão corretas.</li>
+            </ul>
+            <span id="15" class="pergunta">Questão 15: Quais as Zonas existentes de acordo com a classificação das Zonas em
+                áreas classificadas definidas em função da probabilidade da ocorrência (Frequência) da atmosfera
+                explosiva, </span>
+            <ul class="respostas">
+                <li class="errada">Zona 0, Zona I e Zona II (Poeiras e Fibras)</li>
+                <li class="errada">Zona 20, Zona 21 e Zona 22 (Gases e Vapores) </li>
+                <li class="certa">Zona 0, Zona I, Zona II (Gases e Vapores) e Zona 20, Zona 21 e Zona 22 (Poeiras e
+                    Fibras)</li>
+                <li class="errada">Todas as alternativas estão corretas. </li>
+            </ul>
+            <span id="16" class="pergunta">Questão 16: A fonte de liberação é um ponto a partir do qual o produto pode ser
+                liberado para a atmosfera de tal forma que uma atmosfera explosiva pode ser formada, quais são as fontes
+                de liberação existentes?</span>
+            <ul class="respostas">
+                <li class="errada">Fonte de liberação primária, fonte de liberação secundária e fonte de liberação
+                    terciária.</li>
+                <li class="certa">Fonte de liberação primária, fonte de liberação secundária e fonte de liberação
+                    contínua.</li>
+                <li class="errada">Fonte de liberação independente, fonte de liberação intermitente e fonte de
+                    liberação contínua.</li>
+                <li class="errada">Todas as alternativas estão corretas.</li>
+            </ul>
+            <span id="17" class="pergunta">Questão 17: A classe da temperatura é o sistema de classificação de equipamento, com
+                base na sua temperatura máxima de superfície, relacionada com a atmosfera explosiva específica do local
+                onde este será usado, as classes de temperatura são separadas em;</span>
+            <ul class="respostas">
+                <li class="certa">T1, T2, T3, T4, T5 e T6.</li>
+                <li class="errada">Z1, Z2, Z3, Z4, Z5 e Z6.</li>
+                <li class="errada">Primeiro, segundo e terceiro Grau.</li>
+                <li class="errada">Todas as alternativas estão corretas.</li>
+            </ul>
+            <span id="18" class="pergunta">Questão 18: Quais das alternativas abaixo descreve as ações adotadas para controle da
+                atmosfera?</span>
+            <ul class="respostas">
+                <li class="errada">Substituição das fontes geradoras risco por líquidos não inflamáveis ou por
+                    líquidos menos inflamáveis.</li>
+                <li class="errada">Evitar evaporação nos ambientes ou equipamentos fechando os recipientes,
+                    equipamentos, utilizando tetos ou selos flutuantes ou inertizando. </li>
+                <li class="errada">Evitando as faixas de explosividade melhorando a ventilação, coletando poeiras
+                    combustíveis, limpando para evitar acúmulo de poeiras combustíveis.</li>
+                <li class="certa">Todas alternativas estão corretas.</li>
+            </ul>
+            <span id="19" class="pergunta">Questão 19: Quais das alternativas abaixo descreve as ações adotadas para controle da
+                ignição?</span>
+            <ul class="respostas">
+                <li class="certa">Controle do carregamento eletrostático com aterramentos, equipotencialização e
+                    vestimentas Ex.</li>
+                <li class="errada">Exaustores que coletam gases e renovam a atmosfera do ambiente. </li>
+                <li class="errada">Dispor de um PAE (Plano de Atendimento a Emergências), descrevendo quais ações
+                    devem ser adotadas em caso de emergências.</li>
+                <li class="errada">Todas as alternativas estão corretas.</li>
+            </ul>
+            <span id="20" class="pergunta">Questão 20: Marque a alternativa que corresponde corretamente aos conceitos de
+                Controle Coletiva e Controle Individual.</span>
+            <ul class="respostas">
+                <li class="errada">Controle coletivo são as implementações dos equipamentos de proteção coletiva
+                    (EPC), dispositivos utilizados no ambiente de trabalho com o objetivo de proteger os trabalhadores
+                    dos riscos inerentes as atividades.</li>
+                <li class="errada">Controles individuais somente poderão ser adotados quando não for possível tomar
+                    medidas que permitam eliminar os riscos do ambiente. </li>
+                <li class="errada">O equipamento de Proteção Individual (EPI) é todo dispositivo ou produto, de uso
+                    individual utilizado pelo trabalhador, destinado a proteção contra riscos capazes de ameaçar a sua
+                    segurança e a sua saúde.</li>
+                <li class="certa">Todas as alternativas estão corretas.</li>
+            </ul>
+
+
+        </section>
+`
+
+const sectionTrain = document.getElementById('treinamentos');
+sectionTrain.innerHTML = inflamaveis;
