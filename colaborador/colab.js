0338a7f2930ef838d224be51ef3cbb3792fc864e
James-Johny <jamesjohny.sg@gmail.com>
2026-06-30 06:34:14 -0300

    update local 0.08

--- colaborador/colab.js
+++ colaborador/colab.js
@@ -3,6 +3,7 @@ const linkQuimicos = 'https://forms.office.com/Pages/ResponsePage.aspx?id=PK7qOe
 const linkInflamaveis = 'https://forms.office.com/pages/responsepage.aspx?id=PK7qOeaHzkiKJsbGa6eA1o_55B86hohAt7OU0LY97N9UNDE0S1VGQUxPRzA0MElXOTJBQVo3TU9YSiQlQCN0PWcu&route=shorturl';
 const linkNR12 = 'https://forms.office.com/pages/responsepage.aspx?id=PK7qOeaHzkiKJsbGa6eA1o_55B86hohAt7OU0LY97N9UMTRBNlVJTDhZVVg0VjZVU1pKMjBKS1MzRiQlQCN0PWcu&route=shorturl';
 const linkLOTO = 'https://forms.office.com/pages/responsepage.aspx?id=PK7qOeaHzkiKJsbGa6eA1o_55B86hohAt7OU0LY97N9UNVJBTEpYVUpJODIzRDBPWkFKUTFRQUFCNyQlQCN0PWcu&route=shorturl';
+const linkApp = 'https://bit.ly/4eHnpDk'
 
 const lista = document.getElementById('menu-colab');
 lista.innerHTML = `
