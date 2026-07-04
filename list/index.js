f974fbcca6480111341537b7b4e9e239c1bc1868
James-Johny <jamesjohny.sg@gmail.com>
2026-07-04 02:34:59 -0300

    update local 0.16

--- list/index.js
+++
@@ -1,6 +0,0 @@
-function adicionarItem(item) {
-    const item = document.getElementById("item").value
-    const itemList = document.getElementById("lista");
-
-    itemList.innerHTML = `<li>item</li>`
-}
